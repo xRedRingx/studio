633f5b4893e4578835930e8711b5a939ff3b6f71
Liamani El Mahdi <liamanielmahdi@gmail.com>
2025-06-17 22:46:02 +0000

    okay, got this issue in earning page: stitched-error.ts:23  Uncaught Err

--- src/components/barber/ManageSpendingsSection.tsx
+++ src/components/barber/ManageSpendingsSection.tsx
@@ -23,6 +23,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger, // Added missing import
 } from "@/components/ui/alert-dialog";
 import { format, parseISO, isSameDay } from 'date-fns';
 
